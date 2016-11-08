55917c15752945f4a3823cf511ed7c594d804c93
Wenpin Liao <thisisbinbin@gmail.com>
2016-11-08 22:37:41 +0800

    修改ckeditor設定檔

--- ckEditorFileManager/Scripts/ckeditor/config.js
+++ ckEditorFileManager/Scripts/ckeditor/config.js
@@ -3,6 +3,7 @@
     // config.language = 'fr';
     // config.skin = 'kama';
     config.uiColor = '#99CCFF';
+    config.extraPlugins = "html5video";
 
     //video plugin需開啟此設定切換至原始碼模式才不會把內容清除
     config.allowedContent = true;
